eeb7b4e13919831af4286e2fedcf0f68e0f4bb7d
HsojCoding <joshhadderz@gmail.com>
2022-02-09 01:42:02 +0000

    New changes with trandom things learnt

--- html-boilerplate/script.js
+++ html-boilerplate/script.js
@@ -1,14 +1,6 @@
-var coll = document.getElementsByClassName("circle");
-var i;
+const button = document.getElementsByClassName("circle")
+var check = 0
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+button.addEventListener("click"), function open() {
+    console.log("clicked")
 }
